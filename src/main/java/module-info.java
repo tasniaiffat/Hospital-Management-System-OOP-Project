4c1b51629a0b6caf6c9dd1d75aa2723dae9d9a37
tasniaiffat <tasniaiffat@gmail.com>
2023-03-30 22:34:45 +0600

    JavaFX project created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.hospital_management_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.hospital_management_system to javafx.fxml;
+    exports com.example.hospital_management_system;
+}
